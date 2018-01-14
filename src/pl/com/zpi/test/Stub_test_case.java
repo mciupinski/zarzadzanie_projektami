45435a54636e8495dfdaf876f5d966843a09d8bc
eadrwlo <wlodek14a@gmail.com>
2018-01-14 17:54:57 +0100

    First stub test

---
+++ src/pl/com/zpi/test/Stub_test_case.java
@@ -0,0 +1,14 @@
+package pl.com.zpi.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Stub_test_case {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
